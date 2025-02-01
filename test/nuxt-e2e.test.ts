d4527b8fdc2f8c29d94cb777fc0067eb376e551e
Alois Sečkár <ellrohir@seznam.cz>
2025-02-01 18:57:03 +0100

    test: fixed playwright test

--- test/nuxt-e2e.test.ts
+++ test/nuxt-e2e.test.ts
@@ -4,11 +4,7 @@ import { describe, expect, test } from 'vitest'
 describe('NuxtTestComponent E2E test', async () => {
   // setup app.vue in headless browser
   await setup()
-
-  // TODO tests broken
-  // Cannot find module '<local_path>\.nuxt\test\83opp2\output\server\node_modules\vue\server-renderer\index.mjs'
-  //  imported from <local_path>\.nuxt\test\83opp2\output\server\chunks\routes\renderer.mjs
-
+  
   test('component renders in browser', async () => {
     // fetch for the rendered value
     const html = await $fetch('/')
@@ -16,9 +12,10 @@ describe('NuxtTestComponent E2E test', async () => {
   })
 
   test('with playwright', async () => {
+    // render page in headless browser
     const page = await createPage()
     await page.goto(url('/'), { waitUntil: 'hydration' })
-    const text = page.textContent('div')
-    console.log(text)
+    const hasText = await page.getByText('nuxt-spec').isVisible()
+    expect(hasText).toBeTruthy()
   })
 })
