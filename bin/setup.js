5f7bc757063db6b3a11d1f58e1edec0dd66b447b
Alois Sečkár <ellrohir@seznam.cz>
2025-10-06 19:33:56 +0200

    feat: merge CLI execution into one script

--- bin/setup.js
+++ bin/setup.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
-import { promptUser, showMessage } from 'elrh-cosca'
+import { 
+  createFileFromWebTemplate, deletePath, fileExists, hasJsonKey, promptUser,
+  removeFromJsonFile, showMessage, updateConfigFile, updateJsonFile, updateTextFile,
+} from 'elrh-cosca'
 
 /**
  * CLI tool to scaffold necessary adjustments in project folder.
@@ -20,14 +23,80 @@ export async function specSetup() {
   showMessage('This CLI tool will help you include Nuxt Spec in your project.')
   showMessage('Refer to the documentation for more information.', 2)
 
-  const shouldDoAuto = await promptUser('Do you want to set everything up automatically (no more prompts)?')
+  const isAutoRun = await promptUser('Do you want to set everything up automatically (no more prompts)?')
   showMessage('')
 
-  if (shouldDoAuto) {
-    const { specSetupAuto } = await import('./setup-auto.js')
-    await specSetupAuto()
+  // 1) manage dependencies in package.json
+
+  // add nuxt-spec
+  await updateJsonFile('package.json', 'dependencies', {
+    'nuxt-spec': '0.1.9',
+  }, isAutoRun, 'This will add \'nuxt-spec\' dependency to your \'package.json\'. Continue?')
+
+  // remove now obsolete nuxt, vue and vue-router
+  const removeDeps = isAutoRun || await promptUser('As \'nuxt-spec\' provides \'nuxt\', \'vue\' and \'vue-router\' dependencies out of the box, do you want to remove them from your \'package.json\' to avoid duplications and possible version clashes?')
+  if (removeDeps) {
+    if (hasJsonKey('package.json', 'dependencies.nuxt')) {
+      await removeFromJsonFile('package.json', 'dependencies.nuxt', true)
+    }
+    if (hasJsonKey('package.json', 'dependencies.vue')) {
+      await removeFromJsonFile('package.json', 'dependencies.vue', true)
+    }
+    if (hasJsonKey('package.json', 'dependencies.vue-router')) {
+      await removeFromJsonFile('package.json', 'dependencies.vue-router', true)
+    }
+    if (hasJsonKey('package.json', 'devDependencies.nuxt')) {
+      await removeFromJsonFile('package.json', 'devDependencies.nuxt', true)
+    }
+    if (hasJsonKey('package.json', 'devDependencies.vue')) {
+      await removeFromJsonFile('package.json', 'devDependencies.vue', true)
+    }
+    if (hasJsonKey('package.json', 'devDependencies.vue-router')) {
+      await removeFromJsonFile('package.json', 'devDependencies.vue-router', true)
+    }
+  }
+
+  // 2) modify nuxt.config.ts
+  await updateConfigFile('nuxt.config.ts', {
+    extends: [
+      'nuxt-spec',
+    ],
+  }, isAutoRun, 'This will add \'nuxt-spec\' module to your \'nuxt.config.ts\'. Continue?')
+
+  // 3) .npmrc file
+  if (fileExists('.npmrc')) {
+    await updateTextFile('.npmrc', ['shamefully-hoist=true'], isAutoRun, 'This will adjust \'.npmrc\' file in your project. Continue?')
   } else {
-    const { specSetupManual } = await import('./setup-manual.js')
-    await specSetupManual()
+    await createFileFromWebTemplate('https://raw.githubusercontent.com/AloisSeckar/nuxt-spec/refs/heads/main/.npmrc',
+      '.npmrc', isAutoRun, 'This will add \'.npmrc\' file for your project. Continue?')
+  }
+
+  // 4) create vitest.config.ts
+  await createFileFromWebTemplate('https://raw.githubusercontent.com/AloisSeckar/nuxt-spec/refs/heads/main/config/vitest.config.ts.template',
+    'vitest.config.ts', isAutoRun, 'This will create a new \'vitest.config.ts\' file for your project. Continue?')
+
+  // 5) modify scripts in package.json
+  await updateJsonFile('package.json', 'scripts', {
+    'test': 'vitest run',
+    'test-u': 'vitest run -u',
+    'test-i': 'vitest',
+  }, isAutoRun, 'This will adjust the test-related commands in your \'package.json\'. Continue?')
+
+  // 6) clear node_modules and lock file(s)
+  const prepareForReinstall = isAutoRun || await promptUser('Dependencies should be re-installed now. Do you want to remove node_modules and the lock file?')
+  if (prepareForReinstall) {
+    await deletePath('node_modules', true)
+    if (fileExists('package-lock.json')) {
+      await deletePath('package-lock.json', true)
+    }
+    if (fileExists('pnpm-lock.yaml')) {
+      await deletePath('pnpm-lock.yaml', true)
+    }
+    if (fileExists('yarn.lock')) {
+      await deletePath('yarn.lock', true)
+    }
+    if (fileExists('bun.lockb')) {
+      await deletePath('bun.lockb', true)
+    }
   }
 }
