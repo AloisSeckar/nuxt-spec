0e6acaa240be51b00aca7e85a35743ec38499c7c
Alois Sečkár <ellrohir@seznam.cz>
2025-12-14 11:40:21 +0100

    build: bump Vitest, Nuxt + other deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,21 +4,7 @@ export default defineNuxtConfig({
     '@nuxt/test-utils/module',
   ],
 
-  compatibilityDate: '2025-11-04',
-
-  // workaround for https://github.com/nuxt/nuxt/issues/33593
-  typescript: {
-    tsConfig: {
-      include: [
-        '../tests/**/*.ts',
-        '../tests/**/*.spec.ts',
-        '../tests/**/*.test.ts',
-        '../test/**/*.ts',
-        '../test/**/*.spec.ts',
-        '../test/**/*.test.ts',
-      ],
-    },
-  },
+  compatibilityDate: '2025-12-14',
 
   eslint: {
     config: {
