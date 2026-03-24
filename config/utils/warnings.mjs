4264387489c32fe090a7ee1d6727b50862cb2b78
AloisSeckar <alois.seckar@gmail.com>
2026-03-24 15:40:57 +0100

    fix: filtering out stdout messages along with stderr (#36)

--- config/utils/warnings.mjs
+++ config/utils/warnings.mjs
@@ -9,13 +9,19 @@ const messageFilters = [
   '<Suspense> is an experimental feature',
 ]
 
-// 1) filter-out unnecessary stderr logs coming from Vitest
+// 1) filter-out unnecessary stderr/stdout logs coming from Vitest
 // (applied as side-effect on import)
 
 const _stderrWrite = process.stderr.write.bind(process.stderr)
 process.stderr.write = (chunk, ...args) => {
   if (typeof chunk === 'string' && messageFilters.some(f => chunk.includes(f))) return true
   return _stderrWrite(chunk, ...args)
+}
+
+const _stdoutWrite = process.stdout.write.bind(process.stdout)
+process.stdout.write = (chunk, ...args) => {
+  if (typeof chunk === 'string' && messageFilters.some(f => chunk.includes(f))) return true
+  return _stdoutWrite(chunk, ...args)
 }
 
 // 2) filter-out unnecessary console logs coming from Vitest
