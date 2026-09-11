d4351c69fd40e88f04755e0df2b58d63470f1a19
AloisSeckar <alois.seckar@gmail.com>
2026-09-11 20:56:50 +0200

    docs: document difference between mount and mountSuspended (#51)

--- test/nuxt/nuxt-unit.test.ts
+++ test/nuxt/nuxt-unit.test.ts
@@ -12,6 +12,8 @@ const text = 'custom-text'
 
 describe('NuxtSpecTestComponent', () => {
   test('component mounts and renders text properly', () => {
+    // NOTE: plain `mount` would only work if the component does not rely
+    // on Nuxt-specific features (auto-imports, modules, etc.)
     const wrapper = mount(NuxtSpecTestComponent, {
       propsData: {
         text,
@@ -21,6 +23,8 @@ describe('NuxtSpecTestComponent', () => {
   })
 
   test('component mounts using mountSuspended and renders text properly', async () => {
+    // NOTE: using `mountSuspended` will properly initialize all aspects
+    // of the underlying Nuxt app, if component requires it
     const component = await mountSuspended(NuxtSpecTestComponent, {
       props: {
         text,
